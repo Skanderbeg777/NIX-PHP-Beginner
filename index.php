8486e33387fb2fe6edb198d465ec3902f10bad38
Skanderbeg777 <ippoq777@gmail.com>
2020-07-22 17:43:45 +0300

    index.php: add header

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+?>
+
+<html lang="ru">
+<head>
+    <title>NIX Education</title>
+</head>
+<body>
+<header>
+    <nav>
+        <a href="">Урок 1</a>
+        <a href="">Урок 2</a>
+        <a href="">Урок 3</a>
+    </nav>
+</header>
+
+</body>
+</html>
