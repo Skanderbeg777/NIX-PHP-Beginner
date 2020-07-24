204b3b4ffcc5ee1db28da047c67ef3fe20fd3056
Skanderbeg777 <ippoq777@gmail.com>
2020-07-24 13:56:07 +0300

    Created nav.php

--- index.php
+++ index.php
@@ -8,12 +8,7 @@
 </head>
 <body>
 <header>
-    <nav>
-        <a href="index.php" id="home"><img alt="Home ico" src="https://img.icons8.com/nolan/64/home.png"/></a>
-        <a href="">Урок 1</a>
-        <a href="">Урок 2</a>
-        <a href="">Урок 3</a>
-    </nav>
+    <?php require_once "nav.php" ?>
 </header>
 <main>
     <h1>Добро пожаловать</h1>
