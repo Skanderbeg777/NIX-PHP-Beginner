97b208a1d290c7142589006fa4f7e14fbb42472e
Skanderbeg777 <ippoq777@gmail.com>
2020-07-24 13:52:38 +0300

    Add home icon
    * add #home

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
 <header>
     <nav>
+        <a href="index.php" id="home"><img alt="Home ico" src="https://img.icons8.com/nolan/64/home.png"/></a>
         <a href="">Урок 1</a>
         <a href="">Урок 2</a>
         <a href="">Урок 3</a>
