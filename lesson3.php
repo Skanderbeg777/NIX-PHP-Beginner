a80d622638f846836c233bfb20c67e04287b6ddb
Skanderbeg777 <ippoq777@gmail.com>
2020-07-24 16:59:50 +0300

    done lesson3.php

--- lesson3.php
+++ lesson3.php
@@ -1,4 +1,7 @@
 <?php
+$msg = false;
+if (isset($_POST['form']) && strlen($_POST['form']))
+    $msg = strrev($_POST['form']);
 ?>
 
 <html lang="ru">
@@ -11,7 +14,13 @@
     <?php require_once "nav.php" ?>
 </header>
 <main>
-    <h1>Добро пожаловать</h1>
+    <h1>Урок 3</h1>
+    <?= htmlentities($msg); ?>
+    <form method="post">
+        <label for="form">Введите текст</label>
+        <input type="text" name="form" id="form">
+        <input type="submit" value="Отправить">
+    </form>
 </main>
 <?php require_once "footer.php"?>
 </body>
