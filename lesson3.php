cf3ce524e37dece3977d6d35e7dba840a47d3fc4
Skanderbeg777 <ippoq777@gmail.com>
2020-07-24 16:43:02 +0300

    create lesson3.php

---
+++ lesson3.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<html lang="ru">
+<head>
+    <title>NIX Education</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <?php require_once "nav.php" ?>
+</header>
+<main>
+    <h1>Добро пожаловать</h1>
+</main>
+<?php require_once "footer.php"?>
+</body>
+</html>
--- nav.php
+++ nav.php
@@ -2,5 +2,5 @@
     <a href="index.php" id="home"><img alt="Home ico" src="https://img.icons8.com/nolan/64/home.png"/></a>
     <a href="lesson1.php">Урок 1</a>
     <a href="lesson2.php">Урок 2</a>
-    <a href="">Урок 3</a>
+    <a href="lesson3.php">Урок 3</a>
 </nav>
