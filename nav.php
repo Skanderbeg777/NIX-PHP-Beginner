af5bea94e3bb6f93374bb4d9063afc51b6462bc7
Skanderbeg777 <ippoq777@gmail.com>
2020-07-24 13:58:57 +0300

    lesson1.php created

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <nav>
     <a href="index.php" id="home"><img alt="Home ico" src="https://img.icons8.com/nolan/64/home.png"/></a>
-    <a href="">Урок 1</a>
+    <a href="lesson1.php">Урок 1</a>
     <a href="">Урок 2</a>
     <a href="">Урок 3</a>
 </nav>
